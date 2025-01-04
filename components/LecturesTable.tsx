59398b04a4fa93e4c4cf88bba965ade1284dd2b4
jadalkarim <jad_alkarim_al_smail@brown.edu>
2025-01-04 13:44:28 -0500

    add: lecture and assignment tables with CSS

--- components/LecturesTable.tsx
+++ components/LecturesTable.tsx
@@ -1,32 +1,47 @@
-import {FC} from "react";
+import { FC } from "react";
 import Lectures from "../data/lectures.json";
 
-interface LecturesTableProps {
-}
+interface LecturesTableProps {}
 
 const LecturesTable: FC<LecturesTableProps> = () => {
-    return <table className="table">
-        <thead>
-        <tr className="table-header">
-            <th className="pb-3">Topic</th>
-            <th>Date</th>
-            <th>Slides</th>
-            <th>Recording</th>
-        </tr>
-        </thead>
-        <tbody>
-        {Lectures.map(lecture => <tr key={lecture.topic}>
-            <td className="py-3 neon-text-green">{lecture.topic}</td>
-            <td>{lecture.date}</td>
-            <td>{lecture.slidesLink ?
-                <a className="lecture-link" target="_blank" href={lecture.slidesLink}>Slides</a> :
-                <span>-</span>}</td>
-            <td>{lecture.recordingLink ?
-                <a className="lecture-link" target="_blank" href={lecture.recordingLink}>Recording</a> :
-                <span>-</span>}</td>
-        </tr>)}
-        </tbody>
-    </table>
-}
+    return (
+        <table className="lecture-table">
+            <thead>
+                <tr className="lecture-table-header">
+                    <th className="pb-3">Topic</th>
+                    <th>Date</th>
+                    <th>Slides</th>
+                    <th>Recording</th>
+                </tr>
+            </thead>
+            <tbody>
+                {Lectures.map((lecture) => (
+                    <tr key={lecture.topic} className="lecture-table-row">
+                        <td className="py-3 neon-text-green">{lecture.topic}</td>
+                        <td>{lecture.date}</td>
+                        <td>
+                            {lecture.slidesLink ? (
+                                <a className="lecture-link" target="_blank" href={lecture.slidesLink}>
+                                    Slides
+                                </a>
+                            ) : (
+                                <span>-</span>
+                            )}
+                        </td>
+                        <td>
+                            {lecture.recordingLink ? (
+                                <a className="lecture-link" target="_blank" href={lecture.recordingLink}>
+                                    Recording
+                                </a>
+                            ) : (
+                                <span>-</span>
+                            )}
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
+};
 
 export default LecturesTable;
