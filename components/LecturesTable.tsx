3503b159c07b5d5f0810d5735ce7141ca5e4389e
jadalkarim <jad_alkarim_al_smail@brown.edu>
2025-01-05 16:03:26 -0500

    add: arrows indicating links and fix: less glow for assignment table

--- components/LecturesTable.tsx
+++ components/LecturesTable.tsx
@@ -21,8 +21,22 @@ const LecturesTable: FC<LecturesTableProps> = () => {
                         <td>{lecture.date}</td>
                         <td>
                             {lecture.slidesLink ? (
-                                <a className="lecture-link" target="_blank" href={lecture.slidesLink}>
+                                <a className="lecture-link inline-flex items-center" target="_blank" href={lecture.slidesLink}>
                                     Slides
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="h-4 w-4 ml-2 text-teal-400"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth={2}
+                                            d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+                                        />
+                                    </svg>
                                 </a>
                             ) : (
                                 <span>-</span>
@@ -30,8 +44,22 @@ const LecturesTable: FC<LecturesTableProps> = () => {
                         </td>
                         <td>
                             {lecture.recordingLink ? (
-                                <a className="lecture-link" target="_blank" href={lecture.recordingLink}>
+                                <a className="lecture-link inline-flex items-center" target="_blank" href={lecture.recordingLink}>
                                     Recording
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="h-4 w-4 ml-2 text-teal-400"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth={2}
+                                            d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+                                        />
+                                    </svg>
                                 </a>
                             ) : (
                                 <span>-</span>
