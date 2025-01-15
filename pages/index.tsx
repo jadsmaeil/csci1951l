461f97dd0a6a6b48b3a100da291f05f8a4c64b07
Jakobi Haskell <jakobi_haskell@brown.edu>
2025-01-15 13:14:08 +0200

    Creates email link for mph in prof hours

--- pages/index.tsx
+++ pages/index.tsx
@@ -100,16 +100,18 @@ export default function Home() {
                     is appropriate, please reach out to a TA.
                   </p>
                   <p>
-                  You are permitted to use AI tools such as ChatGPT and Copilot 
-                  for high-level conceptual understanding of projects/homeworks 
-                  and small low-level coding details, as long as you are not using 
-                  them to copy-paste large swaths of the assignment or entire written answers.
+                    You are permitted to use AI tools such as ChatGPT and
+                    Copilot for high-level conceptual understanding of
+                    projects/homeworks and small low-level coding details, as
+                    long as you are not using them to copy-paste large swaths of
+                    the assignment or entire written answers.
                   </p>
                   <p>
-                  Note: During interactive grading sessions, TAs reserve the right 
-                  to ask questions about any line of code you have written. 
-                  If you are unable to explain what your code is doing, or your 
-                  explanation is unsatisfactory, there may be disciplinary consequences.
+                    Note: During interactive grading sessions, TAs reserve the
+                    right to ask questions about any line of code you have
+                    written. If you are unable to explain what your code is
+                    doing, or your explanation is unsatisfactory, there may be
+                    disciplinary consequences.
                   </p>
                   <FAQSection question="Projects">
                     <ul className="list-outside pl-8 list-disc">
@@ -145,7 +147,8 @@ export default function Home() {
                         <b className="neon-text-red">completed individually.</b>
                       </li>
                       <li>
-                      You are welcome to ideate with other students, but the words you turn in must be your own.
+                        You are welcome to ideate with other students, but the
+                        words you turn in must be your own.
                       </li>
                       <li>
                         If you’re stuck, you should (1) post on EdStem or (2)
@@ -280,40 +283,43 @@ export default function Home() {
               <CollapsiblePanel title="TA Hours">
                 <div className="space-y-5">
                   <p>
-                  There will be TA hours throughout the week according to the course calendar. 
-                  Some will be remote 1-on-1 TA hours, while others will be in-person collab hours. 
-                  Both are designed to answer HW/Project questions. Please check the Hours website
-                  for the Zoom link/location. 
+                    There will be TA hours throughout the week according to the
+                    course calendar. Some will be remote 1-on-1 TA hours, while
+                    others will be in-person collab hours. Both are designed to
+                    answer HW/Project questions. Please check the Hours website
+                    for the Zoom link/location.
                   </p>
                   <FAQSection question="Remote Hours">
-                    Create a ticket in the queue with a description of your question. 
-                    When you are claimed, join the Zoom link. If multiple people 
-                    have similar questions (whether it's homework or project related), 
-                    we may claim multiple people and explain the question conceptually at 
-                    the same time. 
+                    Create a ticket in the queue with a description of your
+                    question. When you are claimed, join the Zoom link. If
+                    multiple people have similar questions (whether it's
+                    homework or project related), we may claim multiple people
+                    and explain the question conceptually at the same time.
                   </FAQSection>
                   <FAQSection question="Collab Hours">
-                    No need to join the queue, just find the room that hours are hosted in. 
-                    The TA will attempt to group people by similar questions and go around 
-                    the room addressing their questions as a group. Feel free to collaborate 
-                    at a high level or assist with debugging with your peers during these collab hours. 
+                    No need to join the queue, just find the room that hours are
+                    hosted in. The TA will attempt to group people by similar
+                    questions and go around the room addressing their questions
+                    as a group. Feel free to collaborate at a high level or
+                    assist with debugging with your peers during these collab
+                    hours.
                   </FAQSection>
                   <FAQSection question="Professor Hours">
-                    Maurice's hours are available by appointment. Please contact 
-                    him at mph@cs.brown.edu to make an appointment.
+                    Maurice's hours are available by appointment. Please contact
+                    him at <EmailLink email={PROF_EMAIL} /> to make an
+                    appointment.
                   </FAQSection>
                 </div>
               </CollapsiblePanel>
               <CollapsiblePanel title="Can I capstone this course?">
-              Yes. Please read{" "}
-                    <Link
-                      link="https://drive.google.com/file/d/10VWOLpUqv56RJATp_pIFcz-jdYuC9g1Y/view?usp=sharing"
-                      text="this default capstone project description"
-                    />
-                    . Email <EmailLink email={PROF_EMAIL} /> with questions.
+                Yes. Please read{" "}
+                <Link
+                  link="https://drive.google.com/file/d/10VWOLpUqv56RJATp_pIFcz-jdYuC9g1Y/view?usp=sharing"
+                  text="this default capstone project description"
+                />
+                . Email <EmailLink email={PROF_EMAIL} /> with questions.
               </CollapsiblePanel>
             </ContentSection>
-            
 
             {/* Assignments Section */}
             <ContentSection
@@ -338,7 +344,7 @@ export default function Home() {
             <ContentSection title="Lectures" id="lectures" titleColor="green">
               <p className="my-4">
                 Lectures will take place Tuesdays and Thursdays from
-                2:30pm-3:50pm ET in  Salomon Center 003. Recordings will be
+                2:30pm-3:50pm ET in Salomon Center 003. Recordings will be
                 available soon after lecture ends.
               </p>
               {/* <p>
